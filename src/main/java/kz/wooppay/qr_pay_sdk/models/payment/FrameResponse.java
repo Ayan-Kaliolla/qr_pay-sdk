6fa7912a8a8119cd30a4bbf0cd6a0975ffdc946d
AKaliolla <akaliolla@wooppay.com>
2018-02-02 17:39:39 +0600

    test snapshot

--- src/main/java/kz/wooppay/qr_pay_sdk/models/payment/FrameResponse.java
+++ src/main/java/kz/wooppay/qr_pay_sdk/models/payment/FrameResponse.java
@@ -12,6 +12,9 @@ import com.google.gson.annotations.SerializedName;
 
 public class FrameResponse {
 
+    /** payment operation*/
+    private Operation operation;
+
     /** frame url*/
     @SerializedName("iframe")
     private String url;
@@ -24,5 +27,15 @@ public class FrameResponse {
     /** setter for frame url*/
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    /** getter for operation*/
+    public Operation getOperation() {
+        return operation;
+    }
+
+    /** setter for operation*/
+    public void setOperation(Operation operation) {
+        this.operation = operation;
     }
 }
